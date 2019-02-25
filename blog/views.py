cb6f0b7f27fc910daf523f43670d8dfb3cf5f0fd
VIshakBharadwaj94 <vishak.svec@gmail.com>
2019-02-25 23:27:34 +0530

    added forms

--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,11 @@
 from django.shortcuts import render
+from .models import Post # . means from the current package
 # from django.http import HttpResponse (not needed)
 
-posts = [
-
-	{
-
-		'author':'Corey MS',
-		'title':'Blog Post 1',
-		'content':'content 1',
-		'date':'Aug 2018'
-
-	},
-	{
-
-		'author':'Corey MS2',
-		'title':'Blog Post 2',
-		'content':'content 2',
-		'date':'Aug 2019'
-
-	}
-
-]
 
 def home(request):
 
-	context = {'posts':posts}
+	context = {'posts':Post.objects.all()}
 	return render(request,'blog/home.html',context)
 	#context dictionary has to be passed unlike in flask
 
